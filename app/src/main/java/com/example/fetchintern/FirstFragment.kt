2c3772685c52216d019c2bc546a76bc19b53a9d5
Noricks <476852755@QQ.com>
2023-02-05 14:41:28 -0600

    feat: :sparkles: Initial version/skeleton of the program

--- app/src/main/java/com/example/fetchintern/FirstFragment.kt
+++ app/src/main/java/com/example/fetchintern/FirstFragment.kt
@@ -1,12 +1,20 @@
 package com.example.fetchintern
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.fetchintern.databinding.FragmentFirstBinding
+import org.json.JSONArray
+import org.json.JSONException
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -23,18 +31,59 @@ class FirstFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadData()
+    }
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+    private fun loadData() {
+        val queue = Volley.newRequestQueue(binding.root.context)
+        val listItemArray = ArrayList<ListItem>()
+        val url = "https://fetch-hiring.s3.amazonaws.com/hiring.json"
+        val request: StringRequest =
+            @SuppressLint("SetTextI18n")
+            object : StringRequest(Method.GET, url,
+                Response.Listener { response ->
+                    try {
+                        val jsonArray = JSONArray(response)
+                        if (jsonArray.length() > 0) {
+                            for (i in 0 until jsonArray.length()) {
+                                val jsonObject = jsonArray.getJSONObject(i)
+                                val listItem = ListItem(
+                                    jsonObject.getInt("id"),
+                                    jsonObject.getInt("listId"),
+                                    jsonObject.getString("name")
+                                )
+                                listItemArray.add(listItem)
+                            }
+                        }
+                        val inflater = LayoutInflater.from(binding.table.context)
+                        val row = inflater.inflate(R.layout.item_list, null, false)
+                        row.findViewById<TextView>(R.id.id).text = "ID"
+                        row.findViewById<TextView>(R.id.listId).text = "List ID"
+                        row.findViewById<TextView>(R.id.name).text = "Name"
+                        binding.table.addView(row)
+                        for (i in 0 until listItemArray.size) {
+                            val listItem = listItemArray[i]
+                            val row = inflater.inflate(R.layout.item_list, null, false)
+                            row.findViewById<TextView>(R.id.id).text = listItem.id.toString()
+                            row.findViewById<TextView>(R.id.listId).text = listItem.listId.toString()
+                            row.findViewById<TextView>(R.id.name).text = listItem.name
+                            binding.table.addView(row)
+                        }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                }, Response.ErrorListener { error -> // displaying toast message on response failure.
+                    Log.e("tag", "error is " + error!!.message)
+                    Toast.makeText(binding.root.context, "Fail to load data..", Toast.LENGTH_SHORT)
+                        .show()
+                }) {}
+        queue.add(request)
     }
 
     override fun onDestroyView() {
